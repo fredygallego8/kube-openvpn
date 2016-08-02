99a6ac842011cdf282375ca69a403f3b038ce61c
Dan Gillespie <dan@redspread.com>
2016-08-02 14:38:29 -0700

    working DNS and forwarding traffic

--- bin/start.sh
+++ bin/start.sh
@@ -5,25 +5,37 @@ eval $(kube.py)
 
 OPENVPN=/etc/openvpn
 OPENVPN_CFG="$OPENVPN/openvpn.conf"
+OPENVPN_PKI="$OPENVPN/pki"
 
-# Generate OpenVPN config
-ovpn_genconfig -u tcp://$OVPN_HOST
+if [ ! -e "OPENVPN_CFG" ]; then
+    # Add additional configuration if variables are set
+    if [ -n "$KUBE_DNS" ]; then
+        DNS_ARG="-n $KUBE_DNS"
+    fi
 
-# Add additional configuration if variables are set
-if [ -n "$KUBE_SVC_NET" -a -n "$KUBE_SVC_MASK" ]; then
-    printf "push \"route $KUBE_SVC_NET $KUBE_SVC_MASK\"\n" >> $OPENVPN_CFG
+    # Generate OpenVPN config
+    if [ -n "$OVPN_HOST" ]; then
+        ovpn_genconfig -u tcp://$OVPN_HOST $DNS_ARG $ROUTE_ARG $DNS_SEARCH_ARG
+    else
+        printf "ERROR: The environment variable \$OVPN_HOST must be set to the hostname desired for the VPN.\n" 1>&2
+        exit 1
+    fi
+
+    if [ -n "$KUBE_DNS_SEARCH" ]; then
+        printf "push \"dhcp-option SEARCH $KUBE_DNS_SEARCH\"\n" >> $OPENVPN_CFG
+    fi
+
+    if [ -n "$KUBE_SVC_NET" -a -n "$KUBE_SVC_MASK" ]; then
+        printf "push route $KUBE_SVC_NET $KUBE_SVC_MASK\n" >> $OPENVPN_CFG
+    fi
+
+    cat $OPENVPN_CFG
 fi
 
-if [ -n "$KUBE_DNS_SEARCH" ]; then
-    printf "push \"dhcp-option SEARCH $KUBE_DNS_SEARCH\"\n" >> $OPENVPN_CFG
+if [ ! -d "$OPENVPN_PKI" ]; then
+    # Setup CA and other PKI components
+    EASYRSA_BATCH=1 ovpn_initpki nopass
 fi
-
-if [ -n "$KUBE_DNS" ]; then
-    printf "push \"dhcp-option DNS $KUBE_DNS\"\n" >> $OPENVPN_CFG
-fi
-
-# Setup CA and other PKI components
-EASYRSA_BATCH=1 ovpn_initpki nopass
 
 # Start VPN
 ovpn_run
